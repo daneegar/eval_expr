fbf63bc0b818ccb278c239175324074e44d58221
Denis Garifyanov <daneegar@gmail.com>
2019-03-16 14:43:13 +0300

    Initial Commit

---
+++ eval_expr/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  eval_expr
+//
+//  Created by Denis Garifyanov on 16/03/2019.
+//  Copyright © 2019 Denis Garifyanov. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
